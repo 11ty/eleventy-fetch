b207ed2619d7f29dcc44ea50280c6aa30e42e702
Zach Leatherman <zachleatherman@gmail.com>
2021-07-08 20:17:36 -0500

    Try to normalize the path

--- test/AssetCacheTest.js
+++ test/AssetCacheTest.js
@@ -2,10 +2,14 @@ const test = require("ava");
 const path = require("path");
 const AssetCache = require("../src/AssetCache");
 
+function normalizePath(pathStr) {
+  return pathStr.split(path.sep).join("/");
+}
+
 test("Absolute path cache directory", t => {
 	let cache = new AssetCache("lksdjflkjsdf", "/tmp/.cache");
 
-  t.is(cache.cachePath, "/tmp/.cache/eleventy-cache-assets-lksdjflkjsdf");
+  t.is(normalizePath(cache.cachePath), "/tmp/.cache/eleventy-cache-assets-lksdjflkjsdf");
 });
 
 test("Relative path cache directory", t => {
